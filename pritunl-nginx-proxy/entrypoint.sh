3af03386ba4dfda859581c4f3fc5ad1e2c904d95
Aiden Johnson <65328952+tgmstudios@users.noreply.github.com>
2025-04-27 20:30:59 -0400

    delimiter

--- pritunl-nginx-proxy/entrypoint.sh
+++ pritunl-nginx-proxy/entrypoint.sh
@@ -119,7 +119,7 @@ envsubst '${NGINX_PORT} ${PROXY_PASS_DEFAULT} ${PROXY_PORT_DEFAULT} ${ADDITIONAL
     < /etc/nginx/templates/nginx.conf.template > /etc/nginx/nginx.conf.tmp
 
 # Inject dynamic server blocks before closing HTTP block
-sed -i '/^}$/i '"${DYNAMIC_SERVER_BLOCKS}" /etc/nginx/nginx.conf.tmp
+sed -i '/^}$/i '"$(echo "${DYNAMIC_SERVER_BLOCKS}" | sed 's/|/\\|/g')" /etc/nginx/nginx.conf.tmp
 mv /etc/nginx/nginx.conf.tmp /etc/nginx/nginx.conf
 
 log "Final Nginx configuration:"
